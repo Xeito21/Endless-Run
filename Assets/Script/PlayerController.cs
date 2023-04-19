3ddb855ec95c6729dc6b22fdd51b3440300f4671
Xeito <74165796+Xeito21@users.noreply.github.com>
2023-04-19 15:05:00 +0700

    Part 2 done Moving The Player ( moving player left and right )

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     [SerializeField] private float forwardSpeed;
+    private int desiredLane = 1;
+    public float laneDistance = 2.5f; //distance between two lanes
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+
+        //gather the inputs on which lane we should be
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        //calculate where we should be in the future
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane ==2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.fixedDeltaTime);
     }
 
     private void FixedUpdate()
