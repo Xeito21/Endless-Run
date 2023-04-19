f340d51f8be0a7cd83017a54fc0248da388e6b38
Xeito <74165796+Xeito21@users.noreply.github.com>
2023-04-19 14:47:49 +0700

    Part 1 done progress player movement

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    [SerializeField] private float forwardSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+}
